05a3dc77bdf5ef7bf3e12d96f9b599b615a894ac
Deivid <deivid.magno.nascimento@gmail.com>
2021-09-13 22:43:59 -0300

    ensure Remote authentication call HttpClient with correct method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -9,24 +9,24 @@ class RemoteAuthentication {
   RemoteAuthentication({required this.httpClient, required this.url});
 
   Future<void>? auth() async {
-    await httpClient.request(url: url);
+    await httpClient.request(url: url, method: 'post');
   }
 }
 
 abstract class HttpClient {
-  Future<void>? request({required String url}) async {}
+  Future<void>? request({required String url, required method}) async {}
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('Should call Http Client with correct URL', () async {
+  test('Should call Http Client with correct values', () async {
     final httpClient = HttpClientSpy();
     final url = faker.internet.httpUrl();
     //sut = system under test
     final sut = RemoteAuthentication(httpClient: httpClient, url: url);
     await sut.auth();
 
-    verify(httpClient.request(url: url));
+    verify(httpClient.request(url: url, method: 'post'));
   });
 }
